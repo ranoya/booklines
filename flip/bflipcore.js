cb4fed90f80727619d89c9ecdf7040640a91f9c1
Guilherme Ranoya <ranoya@gmail.com>
2026-01-17 01:41:21 -0300

    Revert "espero que eu não tenha feito bobage"
    não deu certo

--- flip/bflipcore.js
+++ flip/bflipcore.js
@@ -439,15 +439,23 @@ let decresce = function () {
   let fut_blBack = document.getElementById("fut_BlocoBack");
   let fut_pgBack = document.getElementById("fut_PaginaBack");
 
-  document.getElementById("fut_PaginaBack").classList.remove("initdouble");
-  document.getElementById("fut_Ifr_PaginaBack").classList.remove("initdouble");
-  document.getElementById("fut_BlocoBack").classList.remove("initdouble");
-  document.getElementById("pas_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pas_Ifr_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pas_BlocoBack").classList.remove("initdouble");
-  document.getElementById("pres_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pres_Ifr_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pres_BlocoBack").classList.remove("initdouble");
+  document.getElementById("fut_PaginaFundo_ESQ").classList.remove("initdouble");
+  document
+    .getElementById("fut_Ifr_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document.getElementById("fut_BlocoFundoE").classList.remove("initdouble");
+  document.getElementById("pas_PaginaFundo_ESQ").classList.remove("initdouble");
+  document
+    .getElementById("pas_Ifr_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document.getElementById("pas_BlocoFundoE").classList.remove("initdouble");
+  document
+    .getElementById("pres_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document
+    .getElementById("pres_Ifr_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document.getElementById("pres_BlocoFundoE").classList.remove("initdouble");
 
   if (posit >= 0) {
     move = setInterval(function () {
@@ -486,11 +494,15 @@ let decresce = function () {
           fut_pgCover.getAttribute("css") != "" &&
           document.getElementById("fut_livro").style.zIndex == 300
         ) {
-          document.getElementById("fut_PaginaBack").classList.add("initdouble");
           document
-            .getElementById("fut_Ifr_PaginaBack")
+            .getElementById("fut_PaginaFundo_ESQ")
             .classList.add("initdouble");
-          document.getElementById("fut_BlocoBack").classList.add("initdouble");
+          document
+            .getElementById("fut_Ifr_PaginaFundo_ESQ")
+            .classList.add("initdouble");
+          document
+            .getElementById("fut_BlocoFundoE")
+            .classList.add("initdouble");
         }
 
         if (
@@ -499,11 +511,15 @@ let decresce = function () {
           pas_pgCover.getAttribute("css") != "" &&
           document.getElementById("pas_livro").style.zIndex == 300
         ) {
-          document.getElementById("pas_PaginaBack").classList.add("initdouble");
           document
-            .getElementById("pas_Ifr_PaginaBack")
+            .getElementById("pas_PaginaFundo_ESQ")
             .classList.add("initdouble");
-          document.getElementById("pas_BlocoBack").classList.add("initdouble");
+          document
+            .getElementById("pas_Ifr_PaginaFundo_ESQ")
+            .classList.add("initdouble");
+          document
+            .getElementById("pas_BlocoFundoE")
+            .classList.add("initdouble");
         }
 
         if (
@@ -513,13 +529,13 @@ let decresce = function () {
           document.getElementById("pres_livro").style.zIndex == 300
         ) {
           document
-            .getElementById("pres_PaginaBack")
+            .getElementById("pres_PaginaFundo_ESQ")
             .classList.add("initdouble");
           document
-            .getElementById("pres_Ifr_PaginaBack")
+            .getElementById("pres_Ifr_PaginaFundo_ESQ")
             .classList.add("initdouble");
           document
-            .getElementById("pres_PaginaBack")
+            .getElementById("pres_PaginaFundo_ESQ")
             .classList.add("initdouble");
         }
         document.getElementById("timeline").style.zIndex = 800;
@@ -548,15 +564,23 @@ let acresce = function () {
   let fut_blBack = document.getElementById("fut_BlocoBack");
   let fut_pgBack = document.getElementById("fut_PaginaBack");
 
-  document.getElementById("fut_PaginaBack").classList.remove("initdouble");
-  document.getElementById("fut_Ifr_PaginaBack").classList.remove("initdouble");
-  document.getElementById("fut_BlocoBack").classList.remove("initdouble");
-  document.getElementById("pas_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pas_Ifr_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pas_BlocoBack").classList.remove("initdouble");
-  document.getElementById("pres_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pres_Ifr_PaginaBack").classList.remove("initdouble");
-  document.getElementById("pres_BlocoBack").classList.remove("initdouble");
+  document.getElementById("fut_PaginaFundo_ESQ").classList.remove("initdouble");
+  document
+    .getElementById("fut_Ifr_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document.getElementById("fut_BlocoFundoE").classList.remove("initdouble");
+  document.getElementById("pas_PaginaFundo_ESQ").classList.remove("initdouble");
+  document
+    .getElementById("pas_Ifr_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document.getElementById("pas_BlocoFundoE").classList.remove("initdouble");
+  document
+    .getElementById("pres_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document
+    .getElementById("pres_Ifr_PaginaFundo_ESQ")
+    .classList.remove("initdouble");
+  document.getElementById("pres_BlocoFundoE").classList.remove("initdouble");
 
   if (posit < docs.length - 2) {
     move = setInterval(function () {
@@ -595,11 +619,13 @@ let acresce = function () {
           fut_pgCover.getAttribute("css") != null &&
           fut_pgCover.getAttribute("css") != ""
         ) {
-          document.getElementById("fut_PaginaBack").classList.add("initdouble");
           document
-            .getElementById("fut_Ifr_PaginaBack")
+            .getElementById("fut_PaginaFundo_ESQ")
             .classList.add("initdouble");
-          document.getElementById("fut_BlocoBack").classList.add("initdouble");
+          document
+            .getElementById("fut_Ifr_PaginaFundo_ESQ")
+            .classList.add("initdouble");
+          document.getElementById("fut_BlocoCover").classList.add("initdouble");
         }
 
         if (
@@ -607,11 +633,15 @@ let acresce = function () {
           pas_pgCover.getAttribute("css") != null &&
           pas_pgCover.getAttribute("css") != ""
         ) {
-          document.getElementById("pas_PaginaBack").classList.add("initdouble");
           document
-            .getElementById("pas_Ifr_PaginaBack")
+            .getElementById("pas_PaginaFundo_ESQ")
             .classList.add("initdouble");
-          document.getElementById("pas_BlocoBack").classList.add("initdouble");
+          document
+            .getElementById("pas_Ifr_PaginaFundo_ESQ")
+            .classList.add("initdouble");
+          document
+            .getElementById("pas_BlocoFundoE")
+            .classList.add("initdouble");
         }
 
         if (
@@ -620,12 +650,14 @@ let acresce = function () {
           pres_pgCover.getAttribute("css") != ""
         ) {
           document
-            .getElementById("pres_PaginaBack")
+            .getElementById("pres_PaginaFundo_ESQ")
             .classList.add("initdouble");
           document
-            .getElementById("pres_Ifr_PaginaBack")
+            .getElementById("pres_Ifr_PaginaFundo_ESQ")
             .classList.add("initdouble");
-          document.getElementById("pres_BlocoBack").classList.add("initdouble");
+          document
+            .getElementById("pres_BlocoFundoE")
+            .classList.add("initdouble");
         }
 
         document.getElementById("timeline").style.zIndex = 800;
