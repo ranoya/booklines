1c0640ca8ef31d96b49e138a162689a9517d9544
Guilherme Ranoya <ranoya@gmail.com>
2026-02-03 10:27:39 -0300

    aplicando transparência no background

--- capas/memorial/mequos.js
+++ capas/memorial/mequos.js
@@ -139,7 +139,7 @@ export default ({
     };
 
     p.setup = function () {
-      p.background(p.cordefundo, 0.7);
+      p.background(p.cordefundo + "44");
       p.createCanvas(
         document.getElementById(el).clientWidth,
         document.getElementById(el).clientHeight,
